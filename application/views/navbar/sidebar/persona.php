a2bbea2f96117c3451363b309a14f5673d52865a
Ade Maman Suherman <dbasedown211@gmail.com>
2023-12-09 17:52:52 +0700

    delete function persona

--- application/views/navbar/sidebar/persona.php
+++ application/views/navbar/sidebar/persona.php
@@ -208,9 +208,44 @@
                                             <td>
                                                 <img class="bd-placeholder-img" width="50" src="<?= base_url('./assets/qrcodes/') . $p['qr']; ?>">
                                             </td>
-                                            <form action="<?= base_url('persona/delete_persona/' . $p['id']) ?>" method="post">
-                                                <td><button type="submit" class="btn btn-warning me-1 mb-1">Delete</button></td>
-                                            </form>
+                                                    <td>
+                                                        <a href="#" class="btn icon btn-info"><i class="bi bi-info-circle"></i></a>
+                                                        <!-- <button type="submit" class="btn btn-warning" onclick="return confirm('Are you sure you want to delete this item?');">Delete</button> -->
+                                                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#danger">Delete</button>
+                                                    </td>
+                                                    <!--Danger theme Modal -->
+                                                    <div class="modal fade text-left" id="danger" tabindex="-1" role="dialog"
+                                                        aria-labelledby="myModalLabel120" aria-hidden="true">
+                                                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+                                                            role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header bg-danger">
+                                                                    <h5 class="modal-title white" id="myModalLabel120">Delete Persona
+                                                                    </h5>
+                                                                    <button type="button" class="close" data-bs-dismiss="modal"
+                                                                        aria-label="Close">
+                                                                        <i data-feather="x"></i>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    Deleting a persona will result in the removal of all user data and experiences associated with that persona.
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-light-secondary"
+                                                                        data-bs-dismiss="modal">
+                                                                        <i class="bx bx-x d-block d-sm-none"></i>
+                                                                        <span class="d-none d-sm-block">Close</span>
+                                                                    </button>
+                                                                    <form action="<?= base_url('persona/delete_persona/' . $p['id']) ?>" method="post">
+                                                                    <button type="submit" class="btn btn-danger ms-1" data-bs-dismiss="modal">
+                                                                        <i class="bx bx-check d-block d-sm-none"></i>
+                                                                        <span class="d-none d-sm-block">Accept</span>
+                                                                    </button>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                         </tr>
                                     <?php endforeach; ?>
                             </tbody>
